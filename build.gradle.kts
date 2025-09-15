46753cdbb9e098cad601f5180fdb5a897d99a3c5
polynomeer <polynomeer@gmail.com>
2025-09-15 09:55:36 +0900

    build: add `sim` module
    - Implemented the initial structure and setup for the `sim` module.
    - Added necessary configurations to integrate the `sim` module into the project.
    - Prepared the module for further development and functionality.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,9 @@
 plugins {
-    kotlin("jvm") version "2.1.10"
+    kotlin("jvm") version "2.0.0" apply false
 }
 
-group = "starkraft"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(21)
+allprojects {
+    repositories {
+        mavenCentral()
+    }
 }
---
+++ sim/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.serialization") version "2.0.0"
+    application
+}
+
+group = "starkraft"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+application {
+    mainClass.set("AppKt")
+}
